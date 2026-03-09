53c02f3ee1d3d18d8950fdc1e8459a57e451d563
BÉDIH <thiagaosantosoliveira2018@gmail.com>
2026-03-09 10:52:20 +0000

    V0
    sementinhaV0
    
    sementinha

---
+++ Controllers/NavegacaoController1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OTAKode.Controllers
+{
+    public class NavegacaoController1 : Controller
+    {
+        public IActionResult Sobre()
+        {
+            ViewData["Message"] = "É um de Venda de MANGÁS";
+            return View();
+        }
+    }
+}
