71551f4c7b9fb6915df363543bb8b98cc55856ff
BÉDIH <thiagaosantosoliveira2018@gmail.com>
2026-03-10 12:54:35 +0000

    login nivel 1

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(LoginViewModel model)
+    {
+        if (model.Email == "admin@email.com" && model.Senha == "123456")
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        ModelState.AddModelError("", "Email ou senha inválidos");
+
+        return View(model);
+    }
+}
---
+++ Controllers/NavegacaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OTAKode.Controllers
+{
+    public class NavegacaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
