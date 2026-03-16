939728d82682ad4635828dfbd708d29ff5f047df
BÉDIH <thiagaosantosoliveira2018@gmail.com>
2026-03-16 10:55:13 +0000

    lojasemView

---
+++ Controllers/LojaController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeuProjeto.Models;
+
+public class LojaController : Controller
+{
+    public IActionResult Index()
+    {
+        var produtos = new List<Produto>
+        {
+            new Produto
+            {
+                IDProduto = 1,
+                Nome = "DanDaDan Vol.1",
+                Autor = "Yukinobu Tatsu",
+                Editora = "Shueisha",
+                Preco = 34.90m,
+                Imagem = "/images/dandadan.jpg"
+            },
+            new Produto
+            {
+                IDProduto = 2,
+                Nome = "Vinland Saga Vol.1",
+                Autor = "Makoto Yukimura",
+                Editora = "Kodansha",
+                Preco = 39.90m,
+                Imagem = "/images/vinland.jpg"
+            },
+            new Produto
+            {
+                IDProduto = 3,
+                Nome = "Evangelion Vol.1",
+                Autor = "Yoshiyuki Sadamoto",
+                Editora = "Kadokawa",
+                Preco = 36.90m,
+                Imagem = "/images/evangelion.jpg"
+            },
+            new Produto
+            {
+                IDProduto = 4,
+                Nome = "Solo Leveling Vol.1",
+                Autor = "Chugong",
+                Editora = "D&C Media",
+                Preco = 42.90m,
+                Imagem = "/images/sololeveling.jpg"
+            },
+            new Produto
+            {
+                IDProduto = 5,
+                Nome = "Berserk Vol.1",
+                Autor = "Kentaro Miura",
+                Editora = "Hakusensha",
+                Preco = 44.90m,
+                Imagem = "/images/berserk.jpg"
+            },
+            new Produto
+            {
+                IDProduto = 6,
+                Nome = "Akira (One Shot)",
+                Autor = "Katsuhiro Otomo",
+                Editora = "Kodansha",
+                Preco = 49.90m,
+                Imagem = "/images/akira.jpg"
+            }
+        };
+
+        return View(produtos);
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,14 @@
+public class Produto
+{
+    public int IDProduto { get; set; }
+
+    public string Nome { get; set; }
+
+    public string Autor { get; set; }
+
+    public string Editora { get; set; }
+
+    public decimal Preco { get; set; }
+
+    public string Imagem { get; set; }
+}
